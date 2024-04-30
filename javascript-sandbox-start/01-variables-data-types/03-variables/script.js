944645dee374457410462ccc312260e4e5b1f41a
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-04-30 17:15:30 +0530

    variables sandbox

--- javascript-sandbox-start/01-variables-data-types/03-variables/script.js
+++ javascript-sandbox-start/01-variables-data-types/03-variables/script.js
@@ -10,3 +10,36 @@
 // first_name    underscore
 // FirstName     PascalCase
 // firstname     owercase
+
+//var has been phased out in favour of const
+//var and let are used more for global scope 
+let firstName = "John"
+let lastName = "Doe"
+console.log(firstName, lastName)
+
+//uninitalized
+let y
+// wont work with const
+//const test
+
+//const cannot be reassigned
+const x=100
+console.log(x)
+// x=99
+// console.log(x)
+
+//objects cannot be reassigned
+const person = {
+    name: "John"
+}
+console.log(person)
+/* wont work
+person = {
+    name: "Test"
+}
+console.log(person) */
+
+//Works
+person.name = "Johnny"
+person.email = "john@email.com"
+console.log(person)
