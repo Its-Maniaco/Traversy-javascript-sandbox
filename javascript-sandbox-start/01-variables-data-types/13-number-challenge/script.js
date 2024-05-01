c405c21544523c3a955c96e2510166eac5f4d0d7
Divya Pamecha <21123621+Its-Maniaco@users.noreply.github.com>
2024-05-01 21:00:55 +0530

    Number challenge

---
+++ javascript-sandbox-start/01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,14 @@
+//random number from 1 to 100
+const x = Math.floor(Math.random()*100+1);
+//random number from 1 to 50
+const y = Math.floor(Math.random()*50+1);
+
+console.log(x, y)
+
+// Operators
+const s=x+y
+console.log(`${x} + ${y} = ${s}`)
+const quot = x/y;
+console.log(`${x} / ${y} = ${quot}`)
+const rem=x%y
+console.log(`${x} % ${y} = ${rem}`)
